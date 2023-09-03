3003661c9c3f1c690ecc621db82e51ba3bfb48d7
Noel Valery <noelrobertovalery@gmail.com>
2023-09-03 12:45:38 -0400

    1

--- main.py
+++ main.py
@@ -18,93 +18,103 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 1920)  # Set width
 cap.set(4, 1080)  # height
 
+frame_count = 0
 while True:
     ret, frame = cap.read()
     if not ret:
         break
+    frame_count += 1
 
-    # Detect objects using MobileNet SSD
-    blob = cv2.dnn.blobFromImage(
-        cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5
-    )
-    net.setInput(blob)
-    detections = net.forward()
-
-    if len(detections) > 0:
-        new_trackers = {}
-
-        for idx in range(detections.shape[2]):
-            confidence = detections[0, 0, idx, 2]
-
-            if confidence > 0.5:
-                class_id = int(detections[0, 0, idx, 1])
-
-                if class_id == 15:
-                    box = detections[0, 0, idx, 3:7] * np.array(
-                        [frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]]
-                    )
-                    (startX, startY, endX, endY) = box.astype("int")
-                    pedestrian = dlib.rectangle(startX, startY, endX, endY)
-
-                    if idx not in trackers:
-                        # Initialize tracker for detected pedestrian
-                        tracker = dlib.correlation_tracker()
-                        tracker.start_track(frame, pedestrian)
-                        new_trackers[idx] = tracker
-
-                        # Generate a random color for each new person
-                        colors[idx] = (
-                            random.randint(0, 255),
-                            random.randint(0, 255),
-                            random.randint(0, 255),
-                        )
-                    else:
-                        new_trackers[idx] = trackers[idx]
-
-        trackers = new_trackers
-
-    for idx, tracker in trackers.items():
-        tracker.update(frame)
-        pos = tracker.get_position()
-        x1, y1, x2, y2 = (
-            int(pos.left()),
-            int(pos.top()),
-            int(pos.right()),
-            int(pos.bottom()),
+    if frame_count % 2 != 0:
+        # Detect objects using MobileNet SSD
+        blob = cv2.dnn.blobFromImage(
+            cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5
         )
-        color = colors[idx]
+        net.setInput(blob)
+        detections = net.forward()
 
-        # Extract the face region if within frame boundaries
-        if y1 >= 0 and y2 < frame.shape[0] and x1 >= 0 and x2 < frame.shape[1]:
-            face_region = frame[y1:y2, x1:x2]
+        if len(detections) > 0:
+            new_trackers = {}
 
-            # Analyze the emotion using deepface
-            dominant_emotion = "None"
-            try:
-                emotion_analysis = DeepFace.analyze(face_region, actions=["emotion"])
+            for idx in range(detections.shape[2]):
+                confidence = detections[0, 0, idx, 2]
 
-                # Get the dominant emotion from the emotion analysis result
-                dominant_emotion = emotion_analysis[0]["dominant_emotion"]
-            except:
-                pass
+                if confidence > 0.5:
+                    class_id = int(detections[0, 0, idx, 1])
 
-                # Draw bounding box and display the dominant emotion analysis
-                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 3)
-                cv2.putText(
-                    frame,
-                    f"Person {idx} - Emotion: {dominant_emotion}",
-                    (x1, y1 - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.9,
-                    color,
-                    2,
-                )
+                    if class_id == 15:
+                        box = detections[0, 0, idx, 3:7] * np.array(
+                            [
+                                frame.shape[1],
+                                frame.shape[0],
+                                frame.shape[1],
+                                frame.shape[0],
+                            ]
+                        )
+                        (startX, startY, endX, endY) = box.astype("int")
+                        pedestrian = dlib.rectangle(startX, startY, endX, endY)
 
-    # Display the frame
-    cv2.imshow("Pedestrian Tracking", frame)
+                        if idx not in trackers:
+                            # Initialize tracker for detected pedestrian
+                            tracker = dlib.correlation_tracker()
+                            tracker.start_track(frame, pedestrian)
+                            new_trackers[idx] = tracker
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+                            # Generate a random color for each new person
+                            colors[idx] = (
+                                random.randint(0, 255),
+                                random.randint(0, 255),
+                                random.randint(0, 255),
+                            )
+                        else:
+                            new_trackers[idx] = trackers[idx]
+
+            trackers = new_trackers
+
+        for idx, tracker in trackers.items():
+            tracker.update(frame)
+            pos = tracker.get_position()
+            x1, y1, x2, y2 = (
+                int(pos.left()),
+                int(pos.top()),
+                int(pos.right()),
+                int(pos.bottom()),
+            )
+            color = colors[idx]
+
+            # Extract the face region if within frame boundaries
+            if y1 >= 0 and y2 < frame.shape[0] and x1 >= 0 and x2 < frame.shape[1]:
+                face_region = frame[y1:y2, x1:x2]
+
+                # Analyze the emotion using deepface
+                dominant_emotion = "None"
+                try:
+                    emotion_analysis = DeepFace.analyze(
+                        face_region, actions=["emotion"]
+                    )
+
+                    # Get the dominant emotion from the emotion analysis result
+                    dominant_emotion = emotion_analysis[0]["dominant_emotion"]
+                except:
+                    pass
+
+                    # Draw bounding box and display the dominant emotion analysis
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), color, 3)
+                    cv2.putText(
+                        frame,
+                        f"Person {idx} - Emotion: {dominant_emotion}",
+                        (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.9,
+                        color,
+                        2,
+                    )
+
+        # Display the frame
+        cv2.imshow("Pedestrian Tracking", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
